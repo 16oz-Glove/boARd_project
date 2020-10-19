eba8144f3ad23ae0ffc45ee3c74559a3d876f935
최영호 <cllisd3782@ajou.ac.kr>
2020-10-19 23:31:32 +0900

    회원가입UI, 로그인errorUI, IntroUI 추가 설계
    [회원가입 UI - joinPanel]
    ( + dActive script 추가)
    *Title_Text : 제목 텍스트
    *email_Input : ID입력하는 부분
    *emailCondition_Text : 이메일상태확인 텍스트
    *Password_Input : 비밀번호 입력 부분
    *PasswordCheck_Input : PW재 입력 부분
    *PasswordCondition_Text : 비밀번호 상태 확인 Text
    *nickName_Input : 사용자 닉네임 입력 부분
    *bar_up : 그냥 UI디자인 부분
    *Button_Cancle : 회원가입 취소 버튼
    (누르면 창이 꺼짐, *onClickButton_unActive 메서드 실행)
    *Button_Create : 회원가입 확인 버튼
    
    [로그인 오류UI - loginError_Panel]
    ( + dActive script 추가)
    *title_Text : 제목 텍스트
    *body_Text : 본문 텍스트
    *exit_Button : OK버턴.(누르면 창이 꺼짐, onClickButton_unActive 메서드 실행)
    [Intro1 UI 구현 - Intro Scene]

---
+++ Assets/Scripts/UI_Script/bActive.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//버튼 누를시, 등록된 오브젝트 활성화 및 비활성화 시키는 스크립트.
+public class bActive : MonoBehaviour
+{
+    public GameObject uiPanel;
+
+    //버튼 누를시, 오브젝트 비활성화
+    public void OnClickButton_unActive()
+    {
+        uiPanel.SetActive(false);
+    }
+
+    //버튼 누를시, 오브젝트 활성화
+    public void OnClickButton_Active()
+    {
+        uiPanel.SetActive(true);
+    }
+
+}
