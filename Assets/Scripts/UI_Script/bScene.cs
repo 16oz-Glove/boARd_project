58e04754e528eea5a84dabd700badcade5b48546
최영호 <cllisd3782@ajou.ac.kr>
2020-10-20 01:58:33 +0900

    Intro1 UI 구현
    dScene  스크립트  :  intro1 화면에서 화면을 터치했을때 다음 Scene(SignIn)으로 전환되도록 하는 스크립트

---
+++ Assets/Scripts/UI_Script/bScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  //씬 관련 메서드 관리 라이브러리
+
+public class bScene : MonoBehaviour
+{
+    //버튼 클릭시, SignIn Scene 로드
+    public void OnclickButton_SceneLoad1()
+    {
+        SceneManager.LoadScene("SignIn");
+    }
+  
+}
