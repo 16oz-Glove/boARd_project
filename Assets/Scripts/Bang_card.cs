121a52dca796d139aade38e04f6f47b0755e9eae
akswb121 <72291089+akswb121@users.noreply.github.com>
2020-10-30 18:56:32 +0900

    Cube오브젝트
    - Cube오브젝트 터치영역 변경
    - Bang_card 스크립트 일부 수정

--- Assets/Scripts/Bang_card.cs
+++ Assets/Scripts/Bang_card.cs
@@ -12,52 +12,48 @@ public class Bang_card : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
+        //animator = GetComponent<Animator>();  //public변수이므로 굳이필요X
     }
 
     // Update is called once per frame
     void Update()
     {
-        touchClick();
+        // 터치 입력이 들어올 경우
+        if (Input.GetMouseButtonDown(0))
+            touchClick();
     }
-
 
 
     // 터치 시 오브젝트 확인 함수
     void touchClick()
     {
 
-        // 터치 입력이 들어올 경우
-        if (Input.GetMouseButtonDown(0))
+        // 오브젝트 정보를 담을 변수 생성
+        RaycastHit hit;
+
+        // 터치 좌표를 담는 변수
+        Ray touchray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // 터치한 곳에 ray를 보냄
+        Physics.Raycast(touchray, out hit);
+
+        // ray가 오브젝트에 부딪힐 경우
+        if (hit.collider != null)
         {
-            // 오브젝트 정보를 담을 변수 생성
-            RaycastHit hit;
-
-            // 터치 좌표를 담는 변수
-            Ray touchray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            // 터치한 곳에 ray를 보냄
-            Physics.Raycast(touchray, out hit);
-
-            // ray가 오브젝트에 부딪힐 경우
-            if (hit.collider != null)
+            if (gDraw == false)
             {
-                if (gDraw == false)
-                {
-                    animator.Play("CubeRotating", -1, 0);   //GUI박스 뜰때 애니메이션 실행
-                    gDraw = true;
-                }
-                else
-                {
-                    gDraw = false;
-                }
-
-                Debug.Log(hit.collider.gameObject.name);    //로그창에 오브젝트 이름 출력
-
-                
+                animator.Play("CubeRotating", -1, 0);   //GUI박스 뜰때 애니메이션 실행
+                gDraw = true;
+            }
+            else
+            {
+                gDraw = false;
             }
 
+            Debug.Log(hit.collider.gameObject.name);    //로그창에 오브젝트 이름 출력
+ 
         }
+
     }
 
     void OnGUI()
