fdb2fc5c8b09f4c3325ce168040964da98427185
akswb121 <72291089+akswb121@users.noreply.github.com>
2020-10-30 15:12:47 +0900

    (임시)Cube 오브젝트 터치 이벤트
    Bang_tt 씬 내의 Cube 오브젝트 클릭(터치)시
    - 로그에 오브젝트 이름 출력
    - 회전 애니메이션 실행
    - 오브젝트 이름 명시된 GUI박스 활성화/비활성화

--- Assets/Scripts/Bang_card.cs
+++ Assets/Scripts/Bang_card.cs
@@ -4,112 +4,69 @@ using UnityEngine;
 
 public class Bang_card : MonoBehaviour
 {
-    public string obj;
-    
-    //bool gDraw = false;
-    //public Vector2 touchpos;
-    //string boxContent;
+    bool gDraw = false;
+    public Vector2 touchpos;
+    string boxContent;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcClick();
+        touchClick();
     }
 
-    void ProcClick()
-    {
-        for (int i = 0; i < Input.touchCount; i++)
-        {
-            ClickPosProc(Input.touches[i].position);
-        }
-    }
 
-    //터치 좌표에 있는 오브젝트를 구한다.
 
-    void ClickPosProc(Vector3 tPos)
+    // 터치 시 오브젝트 확인 함수
+    void touchClick()
     {
 
-        Ray ray = Camera.main.ScreenPointToRay(tPos);
-
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit))
+        // 터치 입력이 들어올 경우
+        if (Input.GetMouseButtonDown(0))
         {
-            obj = hit.collider.gameObject.name;
+            // 오브젝트 정보를 담을 변수 생성
+            RaycastHit hit;
 
-            //터치한 오브젝트 이름 출력
-            print(obj);
+            // 터치 좌표를 담는 변수
+            Ray touchray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            //// Solid_2라는 오브젝트 터치했을 때 애니메이션 수행
-            //if (obj == "Solid_2")
-            //{
+            // 터치한 곳에 ray를 보냄
+            Physics.Raycast(touchray, out hit);
 
-            //    if (sol_2 == true)
-            //    {
-            //        IndyAnim.Play("open");
-            //        sol_2 = false;
-            //    }
-            //    else
-            //    {
-            //        IndyAnim.Play("close");
-            //        sol_2 = true;
-            //    }
-            //}
+            // ray가 오브젝트에 부딪힐 경우
+            if (hit.collider != null)
+            {
+                if (gDraw == false)
+                {
+                    gDraw = true;
+                }
+                else
+                {
+                    gDraw = false;
+                }
+
+                Debug.Log(hit.collider.gameObject.name);    //로그창에 오브젝트 이름 출력
+
+                animator.Play("CubeRotating", -1, 0);
+            }
+
         }
     }
 
+    void OnGUI()
+    {
+        boxContent = "이름\r\n" + gameObject.name;    //오브젝트 이름
 
-            //// 터치 시 오브젝트 확인 함수
-            //void touchClick()
-            //    {
-
-            //        // 터치 입력이 들어올 경우
-            //        if (Input.GetMouseButtonDown(0))
-            //        {
-            //            // 오브젝트 정보를 담을 변수 생성
-            //            RaycastHit hit;
-
-            //            // 터치 좌표를 담는 변수
-            //            Ray touchray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            //            // 터치한 곳에 ray를 보냄
-            //            Physics.Raycast(touchray, out hit);
-
-            //            // ray가 오브젝트에 부딪힐 경우
-            //            if (hit.collider != null)
-            //            {
-            //                if (gDraw == false)
-            //                {
-            //                    gDraw = true;
-            //                }
-            //                else
-            //                {
-            //                    gDraw = false;
-            //                }
-
-            //                Debug.Log(hit.collider.gameObject.name);
-
-            //            }
-
-            //            if (Input.touchCount >= 1)
-            //            {
-            //                touchpos = Input.GetTouch(0).position;
-            //                boxContent = "위치\r\n" + touchpos;
-            //            }
-            //        }
-
-            //        void OnGUI()
-            //        {
-            //            if (gDraw == true)
-            //            {
-            //                GUI.Box(new Rect(10, 10, 100, 100), boxContent);
-            //            }
-            //        }
-            //    }
+        if (gDraw == true)
+        {
+            GUI.Box(new Rect(10, 10, 100, 100), boxContent);
         }
+    }
+    
+}
