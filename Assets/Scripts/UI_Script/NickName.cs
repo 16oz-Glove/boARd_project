bbd21266fd0820985c83e50253e2d377b75cd0e2
acr8286 <acr8286@gmail.com>
2020-11-11 20:24:56 +0900

    메인화면 유저닉네임 출력
    메인화면에 로그인한 유저의 닉네임이 출력되도록 함 (UI_Script 폴더에 NickName 스크립트 작성)

---
+++ Assets/Scripts/UI_Script/NickName.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NickName : MonoBehaviour
+{
+    public Text userName;
+
+    void Start()
+    {
+        userName.GetComponent<Text>().text = AuthManager.User.DisplayName;
+    }
+}
