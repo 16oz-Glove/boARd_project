97eecba091a1af9fdbce1b307b7f7e1a3826abd1
akswb121 <72291089+akswb121@users.noreply.github.com>
2020-11-17 22:18:12 +0900

    패널 텍스트 타이핑이펙트 추가

---
+++ Assets/Scripts/TypingEffect.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypingEffect : MonoBehaviour
+{
+    public string targetMsg;
+    public int CPS; //Char Per Second
+    Text msgText;
+    int index;
+    float interval;
+
+    private void Awake()
+    {
+        msgText = GetComponent<Text>();
+    }
+    
+    public void SetMsg(string msg)
+    {
+        targetMsg = msg;
+        EffectStart();
+    }
+
+
+    void EffectStart()  //시작
+    {
+        msgText.text = "";
+        index = 0;
+
+        interval = 1.0f / CPS;
+        Invoke("Effecting", interval);
+    }
+
+    void Effecting()    //재생
+    {
+        if (msgText.text == targetMsg)
+        {
+            return;
+        }
+        msgText.text += targetMsg[index];
+        index++;
+
+        Invoke("Effecting", interval);   //recursive
+    }
+}
