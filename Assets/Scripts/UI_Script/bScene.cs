11148349a6770e5e88533983bf6ff7ca1f355990
최영호 <cllisd3782@ajou.ac.kr>
2020-11-17 11:09:12 +0900

    GameManager reset 된거 다시 고침
    LobbyScene 오류 수정

--- Assets/Scripts/UI_Script/bScene.cs
+++ Assets/Scripts/UI_Script/bScene.cs
@@ -73,7 +73,7 @@ public class bScene : MonoBehaviour
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
-        Application.Quit() // 어플리케이션 종료
+        Application.Quit(); // 어플리케이션 종료
 #endif
     }
 
