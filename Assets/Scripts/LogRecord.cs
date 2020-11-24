94ce51325a28efb323c0d5bd47420ad66f70c832
acr8286 <acr8286@gmail.com>
2020-11-24 18:36:39 +0900

    로그(튜토리얼) 저장 구현
    LogRecord.cs 작성
    (ARrecognize씬의 reCheckingTutorialBox에서 yes 클릭 시,
    DB에 튜토리얼 로그가 저장됨
    --> 마이페이지에서 출력되는 것을 확인함)

---
+++ Assets/Scripts/LogRecord.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using Firebase.Database;
+
+public class LogRecord : MonoBehaviour
+{
+    // 뱅 튜토리얼 입장 시
+    public void OnClick_tt()
+    {
+        AddLog("tt");
+    }
+
+    // 뱅 연습게임 입장 시
+    public void OnClick_pg()
+    {
+        AddLog("pg");
+    }
+
+    private void AddLog(string type)
+    {
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        string date = DateTime.Now.ToString("yyyyMMdd HH:mm");
+        Logs log = new Logs(date, BoardName.Name_Scene, type); // log setting
+
+        // 로그 개수 읽어오기 --> 키 값
+        string key = AuthManager.User.UserId;
+        FirebaseDatabase.DefaultInstance.GetReference("logs/" + key).GetValueAsync().ContinueWith(task =>
+        {
+            if (task.IsFaulted)
+            {
+                Debug.LogError("Cannot read DB.");
+            }
+            else if (task.IsCompleted)
+            {
+                DataSnapshot snapshot = task.Result;
+                string num = Convert.ToInt32(snapshot.ChildrenCount).ToString();
+                string json = JsonUtility.ToJson(log);
+                reference.Child("logs").Child(key).Child(num).SetRawJsonValueAsync(json); // push log
+                Debug.Log("Succesfully added log to DB.");
+            }
+        });
+    }
+}
