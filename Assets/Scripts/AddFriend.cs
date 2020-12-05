d5a2b766c491fe8e0531f70f782ce1b9d1687087
acr8286 <acr8286@gmail.com>
2020-12-05 17:22:59 +0900

    친구 초대 보강 & 친구 추가하기 구현
    1. MyPageTest.cs: 초대 메세지 기능 보강
    2. AddFriend.cs: 친구 추가하기 스크립트
    3. Content_email.prefab: 친구 추가 시, 검색된 이메일 출력. UI토글로 프리팹 다시 제작.
    4. ToggleControl.cs: 프리팹 내부 스크립트
    5. Friend.cs <-- class for DB
    6. 메인메뉴씬에 complete_Panel UI 제작

--- Assets/Scripts/AddFriend.cs
+++ Assets/Scripts/AddFriend.cs
@@ -1,34 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Firebase.Database;
 
 public class AddFriend : MonoBehaviour
 {
+    public GameObject myPagePanel;
     public GameObject addFriendPanel;
+    public GameObject completePanel;
     public InputField nickNameField;
     public GameObject prefab;
     public GameObject parent;
 
-    private DataSnapshot snapshot;
+    //private DataSnapshot snapshot;
+    private List<IDictionary> snapshotList = new List<IDictionary>();
+    public static string friendEmail;
 
     private void Start()
     {
         addFriendPanel.SetActive(false);
+        completePanel.SetActive(false);
     }
 
     // 마이페이지에서 add 버튼 눌렀을 때 패널 활성화
     public void OnClickAddOfMypage()
     {
         addFriendPanel.SetActive(true);
-        // 활성화되면 이전에 생성해놓은 프리팹 있다면 삭제하는 거 구현해야 함
     }
 
     // 검색하기 버튼 눌렀을 경우
     public void OnClickSearch()
     {
         SearchDB();
-        Invoke("InstantiatePrefab", 1);
+        Invoke("InstantiatePrefab", 1f); // 함수 지연
     }
 
     // 닉네임으로 DB에서 이메일을 검색해라
@@ -42,9 +49,14 @@ public class AddFriend : MonoBehaviour
             }
             else if (task.IsCompleted)
             {
-                snapshot = task.Result;
+                DataSnapshot snapshot = task.Result;
+                foreach (DataSnapshot data in snapshot.Children)
+                {
+                    IDictionary item = (IDictionary)data.Value;
+                    item.Add("uid", data.Key);
+                    snapshotList.Add(item);
+                }
                 Debug.Log("Succesfully searched.");
-                InstantiatePrefab();
             }
         });
     }
@@ -52,20 +64,18 @@ public class AddFriend : MonoBehaviour
     // 프리팹으로 결과 노출하기
     private void InstantiatePrefab()
     {
-        if (snapshot == null)
+        Debug.Log("Instantiating Started.");
+        if (snapshotList == null)
         {
             Debug.Log("There's no such nickName.");
         }
         else
         {
-            foreach (DataSnapshot data in snapshot.Children)
+            for(int i = 0; i < snapshotList.Count; i++)
             {
                 GameObject go = Instantiate(prefab, transform.position, transform.rotation);
                 go.transform.SetParent(parent.transform);
-
-                IDictionary item = (IDictionary)data.Value;
-                Debug.Log(item);
-                go.transform.Find("text").GetComponent<Text>().text = item["email"].ToString();
+                go.transform.Find("Label").GetComponent<Text>().text = snapshotList[i]["email"].ToString();
                 Debug.Log("Instantiated.");
             }
         }
@@ -74,7 +84,56 @@ public class AddFriend : MonoBehaviour
     // 친구 추가 패널에서 add버튼 눌렀을 경우
     public void OnClickAddOfAddFriendPanel()
     {
-        // 이거 프리팹에다 스크립트 추가해서 같이 구현해야 할 듯. 고민해보기.
+        Debug.Log("Checked Email is " + friendEmail);
+        if (friendEmail == null)
+        {
+            return;
+        }
+        
+        // 선택한 이메일의 친구를 추가하라
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+        for (int i = 0; i < snapshotList.Count; i++)
+        {
+            if (snapshotList[i]["email"].ToString() == friendEmail)
+            {
+                // 세팅
+                Friend friend = new Friend(snapshotList[i]["uid"].ToString(), snapshotList[i]["nickName"].ToString());
+
+                // 친구 수 읽어오기 --> 키 값
+                string key = AuthManager.User.UserId;
+                FirebaseDatabase.DefaultInstance.GetReference("users/" + key + "/friends").GetValueAsync().ContinueWith(task =>
+                {
+                    if (task.IsFaulted)
+                    {
+                        Debug.LogError("Cannot read DB.");
+                    }
+                    else if (task.IsCompleted)
+                    {
+                        DataSnapshot snapshot = task.Result;
+                        string num = Convert.ToInt32(snapshot.ChildrenCount).ToString(); // 친구 수
+                        string json = JsonUtility.ToJson(friend);
+                        reference.Child("users").Child(key).Child("friends").Child(num).SetRawJsonValueAsync(json); // push friend
+                        Debug.Log("Succesfully added friend to DB.");
+                    }
+                });
+            }
+        }
+        Debug.Log("Let pages be non-active");
+        completePanel.SetActive(true);
+        addFriendPanel.SetActive(false);
+        myPagePanel.SetActive(false);
     }
 
+    public void OnClosePage()
+    {
+        nickNameField.text = null;
+
+        // 내부 프리팹 삭제
+        for (int i = 0; i < parent.transform.childCount; i++)
+        {
+            Destroy(parent.transform.GetChild(i).gameObject);
+        }
+    }
 }
+
+
